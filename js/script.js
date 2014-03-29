fac85c4224b37ed183e7cadad16b56ff6096f1be
LeedleDev <dwitt@dranweb.com>
2014-03-28 21:08:19 -0400

    Nav Fixes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-// JavaScript Document
 $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
  
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 500,
+      paginationSpeed : 400,
+      singleItem:true
+	  
  
-      items : 4,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
  
   });
  
 });
+
